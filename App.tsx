870d7454665b97498f983375940b70cd22222ccd
Gihan <rangana@tangent.lk>
2021-02-17 11:41:40 +0530

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs-react-native';
+import * as posenet from '@tensorflow-models/posenet';
+
 
 export default function App() {
+
+  const [isTFReady,setIsTFReady] = useState(false);
+  const [isPosenetReady,setIsPosenetReady] = useState(false);
+
+  let model:any;
+
+  const onLoad = async () => {
+    await tf.ready();
+    setIsTFReady(true);
+
+    model = await posenet.load();
+    setIsPosenetReady(true);
+  }
+
+  useEffect(() => {
+    
+    onLoad();
+
+  },[onLoad]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>
+
+        TF Ready? {isTFReady ? 'Yes' : 'Loading...'}
+        
+      </Text>
+      <Text>
+
+        Posenet Ready? {isPosenetReady ? 'Yes' : 'Loading...'}
+        
+      </Text>
     </View>
   );
 }
